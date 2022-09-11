399774f753ed5f0fbb6fb0ed2d824ac99aa92beb
Anthony <antonio4793@hotmail.com>
2022-09-11 12:01:37 -0500

    Nivel 1: Primera solucion aplicando TDD

--- cupon-app/src/main/java/com/meli/cupon/domain/services/CouponService.java
+++ cupon-app/src/main/java/com/meli/cupon/domain/services/CouponService.java
@@ -1,7 +1,6 @@
 package com.meli.cupon.domain.services;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CouponService {
 
@@ -9,11 +8,24 @@ public class CouponService {
      * Encuentra la lista de productos favoritos mas costosa que se puede comprar con un cupon
      * sin superar su valor
      *
-     * @param items la lista de productos favoritos de un cliente
-     * @param amount es el valor del cupon
+     * @param items Lista de productos favoritos de un cliente con su respectivos valor
+     * @param amount Valor del cupon
      * @return la lista de productos favoritos mas costosa que se puede comprar con un cupon
      */
     List<String> calculate(Map<String, Float> items, Float amount) {
-        return List.of("");
+
+        final Map.Entry<String, Float> productosComprables = items.entrySet().stream()
+            .filter(producto -> producto.getValue() <= amount)
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .reduce(Map.entry("", 0f), (productosFavoritosMasCaros, siguienteProductoFavorito) -> {
+                float valorProductosFavoritosMasCaros = productosFavoritosMasCaros.getValue() + siguienteProductoFavorito.getValue();
+                return valorProductosFavoritosMasCaros <= amount ?
+                    Map.entry(productosFavoritosMasCaros.getKey() + siguienteProductoFavorito.getKey() + ",", valorProductosFavoritosMasCaros) :
+                    productosFavoritosMasCaros;
+            });
+
+        return Objects.equals(productosComprables.getKey(), "") ?
+            List.of() :
+            Arrays.stream(productosComprables.getKey().split(",")).toList();
     }
 }
--- cupon-app/src/test/groovy/com/meli/cupon/domain/services/CouponServiceSpec.groovy
+++ cupon-app/src/test/groovy/com/meli/cupon/domain/services/CouponServiceSpec.groovy
@@ -4,13 +4,20 @@ import spock.lang.Specification
 
 class CouponServiceSpec extends Specification {
 
-    def "primer tests con spock"() {
+    def "cuando el cliente tiene no tiene ningun producto favorito"() {
+        given: "ningun productos favorito"
+        Map<String, Float> productosFavoritos = Map.of()
+
+        when:
+        def productosMasCaros = new CouponService().calculate(productosFavoritos, 100.00f)
+
+        then:
+        productosMasCaros == []
+    }
+
+    def "cuando el cliente tiene un unico producto favorito"() {
         given: "productos favoritos con su precio"
-        def productosFavoritos = [producto1: precioProducto1,
-                                                          producto2: precioProducto2,
-                                                          producto3: precioProducto3,
-                                                          producto4: precioProducto4,
-                                                          producto5: precioProducto5]
+        def productosFavoritos = ["MLA1": precioProducto1]
 
         when:
         def productosMasCaros = new CouponService().calculate(productosFavoritos, valorCupon)
@@ -19,11 +26,31 @@ class CouponServiceSpec extends Specification {
         productosMasCaros == productosEsperados
 
         where:
-        producto1 | precioProducto1 | producto2 | precioProducto2 | producto3 | precioProducto3 | producto4 | precioProducto4 | producto5 | precioProducto5 | valorCupon || productosEsperados
-        "MLA1"    | 100             | "MLA2"    | 210             | "MLA3"    | 260             | "MLA4"    | 80              | "MLA5"    | 90              | 100        || [""]
-        "MLA1"    | 100             | "MLA2"    | 210             | "MLA3"    | 260             | "MLA4"    | 80              | "MLA5"    | 90              | 100        || [""]
-        "MLA1"    | 100             | "MLA2"    | 210             | "MLA3"    | 260             | "MLA4"    | 80              | "MLA5"    | 90              | 100        || ["1"]
-        "MLA1"    | 100             | "MLA2"    | 210             | "MLA3"    | 260             | "MLA4"    | 80              | "MLA5"    | 90              | 100        || [""]
+        precioProducto1 | valorCupon || productosEsperados
+        99.99f          | 100.00f    || ["MLA1"]
+        100.00f         | 100.00f    || ["MLA1"]
+        100.01f         | 100.00f    || []
+    }
 
+    def "primer tests con spock"() {
+        given: "productos favoritos con su precio"
+        def productosFavoritos = ["MLA11": precioProducto1,
+                                  "MLA12": precioProducto2,
+                                  "MLA13": precioProducto3,
+                                  "MLA14": precioProducto4,
+                                  "MLA15": precioProducto5]
+
+        when:
+        def productosMasCaros = new CouponService().calculate(productosFavoritos, valorCupon)
+
+        then:
+        productosMasCaros == productosEsperados
+
+        where:
+        precioProducto1 | precioProducto2 | precioProducto3 | precioProducto4 | precioProducto5 | valorCupon || productosEsperados
+        100f            | 210f            | 260f            | 80f             | 90f             | 500f       || ["MLA1”, “MLA2”, “MLA4”, “MLA5"]
+        100f            | 210f            | 260f            | 80f             | 90f             | 100f       || ["MLA1”, “MLA2”, “MLA4”, “MLA5"]
+        100f            | 210f            | 260f            | 80f             | 90f             | 100f       || ["MLA1”, “MLA2”, “MLA4”, “MLA5"]
+        100f            | 210f            | 260f            | 80f             | 90f             | 100f       || ["MLA1”, “MLA2”, “MLA4”, “MLA5"]
     }
 }
